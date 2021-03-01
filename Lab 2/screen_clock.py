b8fbf2ccc0cd62bbc7652db63b72fb24001e135f
beibeilu <bl643@cornell.edu>
2021-03-01 11:38:38 -0500

    modify screen_clock.py

--- Lab 2/screen_clock.py
+++ Lab 2/screen_clock.py
@@ -1,4 +1,5 @@
 import time
+from datetime import datetime
 import subprocess
 import digitalio
 import board
@@ -60,13 +61,76 @@ font = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf", 18)
 backlight = digitalio.DigitalInOut(board.D22)
 backlight.switch_to_output()
 backlight.value = True
+buttonA = digitalio.DigitalInOut(board.D23)
+buttonB = digitalio.DigitalInOut(board.D24)
+buttonA.switch_to_input()
+buttonB.switch_to_input()
+
+# Helpers
+def to_time(value: str) -> int:
+	dt = datetime.strptime(value, '%Y-%m-%d %H:%M:%S')
+	return dt
+
+def strfdelta(tdelta, fmt):
+	d = {"days": tdelta.days}
+	d["hours"], rem = divmod(tdelta.seconds, 3600)
+	d["minutes"], d["seconds"] = divmod(rem, 60)
+	return fmt.format(**d)
+
+countdowns = {
+	"Wellness Day": "2021-03-09 00:00:00",
+	"Graduation Day": "2021-05-26 10:00:00"
+}
+
+def part_of_day():
+	hour = datetime.now().hour
+	return ("morning" if 5 <= hour <= 11
+                else
+                    "afternoon" if 12 <= hour <= 17
+                else
+    	            "evening" if 18 <= hour <= 22
+	    	else
+	            "night"
+	)
+
+index = 0
+
+def show_countdown(index):
+	delta = to_time(list(countdowns.values())[index]) - datetime.now()
+	line1 = strfdelta(delta, "{days} days {hours} hours and {minutes} min")
+	line2 = "until " + list(countdowns)[index]
+	return (line1, line2)
+    
+#show_countdown(index)
 
 while True:
     # Draw a black filled box to clear the image.
     draw.rectangle((0, 0, width, height), outline=0, fill=0)
+    
+    # Show current time
+    t = time.strftime("%m/%d/%Y %H:%M:%S")
+    y = top
+    draw.text((x, y), t, font=font, fill="#FFFFFF")
+    greeting = "Good " + part_of_day() + "!"
+    y += font.getsize(t)[1]
+    draw.text((x, y), greeting, font=font, fill="#FFFF00")
+    
+    if buttonA.value and not buttonB.value:
+        draw.rectangle((0, 0, width, height), outline=0, fill=0)
+        text = show_countdown(0)
+        y = top
+        draw.text((x, y), text[0], font=font, fill="#FFFFFF")
+        y += font.getsize(text[0])[1]
+        draw.text((x, y), text[1], font=font, fill="#FFFFFF")
 
-    #TODO: fill in here. You should be able to look in cli_clock.py and stats.py 
-
-    # Display image.
+    if buttonB.value and not buttonA.value:
+        draw.rectangle((0, 0, width, height), outline=0, fill=0)
+        text = show_countdown(1)
+        y = top
+        draw.text((x, y), text[0], font=font, fill="#FFFFFF")
+        y += font.getsize(text[0])[1]
+        draw.text((x, y), text[1], font=font, fill="#FFFFFF")        
+    
+    # Display image
     disp.image(image, rotation)
     time.sleep(1)
